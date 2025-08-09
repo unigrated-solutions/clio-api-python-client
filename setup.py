7befaa15a1d0b8b1aab04d56b44fce136daa52a6
a1-richmond <dev@unigratedpartners.com>
2025-08-09 01:07:22 -0400

    v0.1.1

--- setup.py
+++ setup.py
@@ -10,11 +10,11 @@ long_description = (project_root / "README.md").read_text()
 
 setup(
     name="clio-manage-api-client",
-    version="0.1.0",
+    version="0.1.1",
     author="Unigrated Partners",
     author_email="dev@unigratedpartners.com",
-    description="An unofficial Python client for the Clio Manage API.",
-    keywords="clio, manage, api, legal, client, sdk, integration, unigration",
+    description="Python Client for the Clio Manage API.",
+    keywords="clio python client, clio manage api, legal, client, sdk, integration, unigration",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/unigrated-solutions/clio-api-python-client",
